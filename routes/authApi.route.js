0148a52a2304e2cf48a43f1d5d4c759f37088e6a
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-02-06 19:02:10 -0400

    created encrypt password on register

--- routes/authApi.route.js
+++ routes/authApi.route.js
@@ -10,30 +10,29 @@ router.get("/login", (req, res) => {
     res.json({"message": "Login page"});
 })
 
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get("/register", (req, res) => {
     // res.send("<h1>Login page</h1>");
     res.json({"message": "Register page"});
 })
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 
     // get form elements
     let formContents = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
+        
     };
 
     // compare passwords
-    if (formContents.password!==formContents.confirmPassword) {
+    if (req.body.password!==req.body.confirmPassword) {
         return res.status(400).json({message: "Error. Passwords don't match"});
     }
 
-    // encrypt password
     
 
     const findUser = User.findOne({
@@ -41,15 +40,19 @@ router.post("/register", (req, res) => {
     }) 
 
     // check if the user already exists
-    findUser.then((data) => {
+    await findUser.then((data) => {
         if (data) {
             return res.status(400).json({message: "Could not create user, user already exists"});
         } else {
-            User.create({
-                email: formContents.email,
-                firstName: formContents.firstName,
-                lastName: formContents.lastName,
-                password: formContents.password,
+
+            // encrypt password
+            bcrypt.hash(req.body.password, saltRounds, function(error, hash) {
+                User.create({
+                    email: formContents.email,
+                    firstName: formContents.firstName,
+                    lastName: formContents.lastName,
+                    password: hash
+                });
             });
             return res.status(201).json({message: "User added to db"});
         }
