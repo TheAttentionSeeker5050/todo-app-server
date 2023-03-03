d3e45281131b42f911322687c18f9f15d4c86377
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-03-03 11:30:04 -0400

    .

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const cors = require('cors');
 const whiteList = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    "https://todo-app-5645.herokuapp.com/",
+    "https://todo-app-5645.herokuapp.com",
 ]
 
 // use cors functions
